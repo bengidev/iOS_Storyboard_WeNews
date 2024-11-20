6bed95c93e3e0a163a806c9b42afcc62093533f6
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2024-11-20 13:26:57 +0700

    chore: change UILabel font to resize dynamically

--- WeNews/WeNews/Extensions/UILabelExtension.swift
+++ WeNews/WeNews/Extensions/UILabelExtension.swift
@@ -10,35 +10,23 @@ import UIKit
 
 extension UILabel {
     func loadAppTitleLabelStyle() {
-        switch Device.current.diagonal {
-        case ...Device.iPhoneSE.diagonal:
-            font = .preferredFont(forTextStyle: .headline)
-        case ...Device.iPhoneSE3.diagonal:
-            font = .preferredFont(forTextStyle: .title1).bold
-        default:
-            font = .preferredFont(forTextStyle: .largeTitle).bold
-        }
+        guard let customFont = UIFont(name: "Georgia", size: Device.current.diagonal * 6) else { return }
+        let fontMetrics = UIFontMetrics(forTextStyle: .largeTitle)
+        self.font = fontMetrics.scaledFont(for: customFont)
+        self.adjustsFontForContentSizeCategory = true
     }
 
     func loadAppBodyLabelStyle() {
-        switch Device.current.diagonal {
-        case ...Device.iPhoneSE.diagonal:
-            font = .preferredFont(forTextStyle: .body).bold
-        case ...Device.iPhoneSE3.diagonal:
-            font = .preferredFont(forTextStyle: .subheadline).bold
-        default:
-            font = .preferredFont(forTextStyle: .headline)
-        }
+        guard let customFont = UIFont(name: "AppleSDGothicNeo-Regular", size: Device.current.diagonal * 2.5) else { return }
+        let fontMetrics = UIFontMetrics(forTextStyle: .headline)
+        self.font = fontMetrics.scaledFont(for: customFont)
+        self.adjustsFontForContentSizeCategory = true
     }
-    
+
     func loadAppFootnoteLabelStyle() {
-        switch Device.current.diagonal {
-        case ...Device.iPhoneSE.diagonal:
-            font = .preferredFont(forTextStyle: .caption1)
-        case ...Device.iPhoneSE3.diagonal:
-            font = .preferredFont(forTextStyle: .footnote)
-        default:
-            font = .preferredFont(forTextStyle: .subheadline)
-        }
+        let customFont: UIFont = .italicSystemFont(ofSize: Device.current.diagonal * 2.5)
+        let fontMetrics = UIFontMetrics(forTextStyle: .footnote)
+        self.font = fontMetrics.scaledFont(for: customFont)
+        self.adjustsFontForContentSizeCategory = true
     }
 }
