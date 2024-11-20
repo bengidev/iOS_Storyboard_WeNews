32aa1d639c33b40453f7f876c1f51a0adac57cf2
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2024-11-20 16:31:46 +0700

    chore: add CategoryNews API model to project

---
+++ WeNews/WeNews/DataSources/CurrentsAPISource/CategoryNews.swift
@@ -0,0 +1,57 @@
+//
+//  CategoryNews.swift
+//  WeNews
+//
+//  Created by ENB Mac Mini M1 on 20/11/24.
+//
+
+import Foundation
+
+enum CategoryNews: String, Equatable {
+    case regional
+    case technology
+    case lifestyle
+    case business
+    case general
+    case programming
+    case science
+    case entertainment
+    case world
+    case sports
+    case finance
+    case academia
+    case politics
+    case health
+    case opinion
+    case food
+    case game
+    case fashion
+    case academic
+    case crap
+    case travel
+    case culture
+    case economy
+    case environment
+    case art
+    case music
+    case notsure
+    case CS
+    case education
+    case redundant
+    case television
+    case commodity
+    case movie
+    case entrepreneur
+    case review
+    case auto
+    case energy
+    case celebrity
+    case medical
+    case gadgets
+    case design
+    case EE
+    case security
+    case mobile
+    case estate
+    case funny
+}
