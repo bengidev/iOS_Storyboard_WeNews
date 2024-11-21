737d70561c0ba02e169ddd4f3ed1d3c5468b8cb1
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2024-11-21 16:41:18 +0700

    chore: add SearchNews to models in Home feature

---
+++ WeNews/WeNews/Features/HomeScreen/Models/SearchNews.swift
@@ -0,0 +1,42 @@
+//
+//  SearchNews.swift
+//  WeNews
+//
+//  Created by ENB Mac Mini M1 on 21/11/24.
+//
+
+import UIKit
+
+// MARK: - SearchNews
+
+struct SearchNews: Identifiable, Sendable, Equatable {
+    let id: UUID = .init()
+
+    let image: UIImage
+    let title: String
+    let body: String
+}
+
+extension SearchNews {
+    func with(
+        image: UIImage? = nil,
+        title: String? = nil,
+        body: String? = nil
+    ) -> SearchNews {
+        return SearchNews(
+            image: image ?? self.image,
+            title: title ?? self.title,
+            body: body ?? self.body
+        )
+    }
+}
+
+// MARK: SearchNews.CodingKeys
+
+extension SearchNews {
+    static let empty: SearchNews = .init(image: .init(), title: .init(), body: .init())
+
+    static func == (lhs: SearchNews, rhs: SearchNews) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
