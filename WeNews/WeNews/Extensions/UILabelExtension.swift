7b267a51c13cec8ecc8a9b49224bcb542876168c
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2024-11-21 09:09:16 +0700

    chore: change separate font builder to one

--- WeNews/WeNews/Extensions/UILabelExtension.swift
+++ WeNews/WeNews/Extensions/UILabelExtension.swift
@@ -9,23 +9,13 @@ import DeviceKit
 import UIKit
 
 extension UILabel {
-    func loadAppTitleLabelStyle() {
-        guard let customFont = UIFont(name: "Georgia", size: Device.current.diagonal * 6.5) else { return }
-        let fontMetrics = UIFontMetrics(forTextStyle: .largeTitle)
-        self.font = fontMetrics.scaledFont(for: customFont)
-        self.adjustsFontForContentSizeCategory = true
-    }
-
-    func loadAppBodyLabelStyle() {
-        guard let customFont = UIFont(name: "AppleSDGothicNeo-Regular", size: Device.current.diagonal * 2.5) else { return }
-        let fontMetrics = UIFontMetrics(forTextStyle: .headline)
-        self.font = fontMetrics.scaledFont(for: customFont)
-        self.adjustsFontForContentSizeCategory = true
-    }
-
-    func loadAppFootnoteLabelStyle() {
-        let customFont: UIFont = .italicSystemFont(ofSize: Device.current.diagonal * 2.5)
-        let fontMetrics = UIFontMetrics(forTextStyle: .footnote)
+    func loadAppLabelStyle(
+        withFontName fontName: String = "Apple SD Gothic Neo",
+        witSize size: CGFloat = 17.0,
+        withTextStyle style: UIFont.TextStyle = .body
+    ) {
+        guard let customFont = UIFont(name: fontName, size: size) else { return }
+        let fontMetrics = UIFontMetrics(forTextStyle: style)
         self.font = fontMetrics.scaledFont(for: customFont)
         self.adjustsFontForContentSizeCategory = true
     }
