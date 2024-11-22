20dcae4dfc844c4f7b3f75b4301aea6f4179e05c
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2024-11-22 16:36:24 +0700

    chore:add method usage descriptions in coordinator

--- WeNews/WeNews/Coordinators/BaseCoordinator.swift
+++ WeNews/WeNews/Coordinators/BaseCoordinator.swift
@@ -20,30 +20,52 @@ class BaseCoordinator: Coordinator {
 
     // MARK: Functions
 
-    func start() { fatalError("Start method must be implemented") }
+    /// Implements the concrete start method in related coordinator
+    ///
+    func start() { fatalError("start method must be implemented") }
 
+    /// Appends child coordinator and set the parent coordinator
+    ///
+    /// This method usually used in conjunction to show related feature
+    ///
     func start(coordinator: Coordinator) {
         self.childCoordinators.append(coordinator)
 
         coordinator.parentCoordinator = self
         coordinator.start()
 
-        dump(self.childCoordinators.description, name: "BaseCoordinator start")
+        dump(self.parentCoordinator.debugDescription, name: "start parent")
+        dump(self.childCoordinators.debugDescription, name: "start coordinators")
     }
 
+    /// Removes all child coordinators from parent
+    ///
+    /// This methods usually used when switch to another feature
+    ///
     func removeChildCoordinators() {
+        dump(self.childCoordinators.debugDescription, name: "removeChildCoordinators before")
+
         for childCoordinator in self.childCoordinators {
             childCoordinator.removeChildCoordinators()
         }
 
         self.childCoordinators.removeAll()
+
+        dump(self.childCoordinators.debugDescription, name: "removeChildCoordinators after")
     }
 
+    /// Removes latest child coordinator from parent
+    ///
+    /// This method usually used when interaction with current
+    /// child coordinator finish
+    ///
     func didFinish(coordinator: Coordinator) {
         if let index = self.childCoordinators.firstIndex(where: { $0 === coordinator }) {
+            dump(self.childCoordinators.debugDescription, name: "didFinish before")
+
             self.childCoordinators.remove(at: index)
 
-            dump(self.childCoordinators.description, name: "BaseCoordinator didFinish")
+            dump(self.childCoordinators.debugDescription, name: "didFinish after")
         }
     }
 }
