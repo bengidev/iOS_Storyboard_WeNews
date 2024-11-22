bece1e174138af94aac96e94a4265fbe1730bdf8
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2024-11-22 16:41:06 +0700

    chore: add MainViewModel in MainScreen feature

---
+++ WeNews/WeNews/Features/MainScreen/Models/MainScreenType.swift
@@ -0,0 +1,13 @@
+//
+//  MainScreenType.swift
+//  WeNews
+//
+//  Created by ENB Mac Mini M1 on 22/11/24.
+//
+
+import Foundation
+
+enum MainScreenType: Equatable, CaseIterable {
+    case home
+    case favorite
+}
