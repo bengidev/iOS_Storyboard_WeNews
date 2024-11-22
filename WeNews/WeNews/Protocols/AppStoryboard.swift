75796708c3b2be0a78e35bb4a5fc3e1324e58e2d
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2024-11-22 16:43:14 +0700

    fix: add default implementations in AppStoryboard

--- WeNews/WeNews/Protocols/AppStoryboard.swift
+++ WeNews/WeNews/Protocols/AppStoryboard.swift
@@ -7,9 +7,22 @@
 
 import UIKit
 
+// MARK: - AppStoryboard
+
 protocol AppStoryboard {
     static var id: String { get }
     static var name: String { get }
-    
+
     static func generateController() -> (AppStoryboard & UIViewController)?
+    static func generateTabBarController() -> (AppStoryboard & UITabBarController)?
+}
+
+extension AppStoryboard {
+    static func generateTabBarController() -> (AppStoryboard & UITabBarController)? {
+        fatalError("generateTabBarController method must be implemented")
+    }
+
+    static func generateController() -> (AppStoryboard & UIViewController)? {
+        fatalError("generateController method must be implemented")
+    }
 }
