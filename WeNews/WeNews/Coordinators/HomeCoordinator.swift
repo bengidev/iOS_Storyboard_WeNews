a0d9f70af91aa46ae61e206078d85fd8f078818f
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2024-11-25 16:44:09 +0700

    refactor: inject viewModel from Main coordinator

--- WeNews/WeNews/Coordinators/HomeCoordinator.swift
+++ WeNews/WeNews/Coordinators/HomeCoordinator.swift
@@ -15,7 +15,10 @@ class HomeCoordinator: BaseCoordinator {
 
     // MARK: Properties
 
+    weak var viewModel: HomeViewModel?
+
     private let disposeBag = DisposeBag()
+    private let apiSource = CurrentsAPISource.instance
 
     // MARK: Lifecycle
 
@@ -24,15 +27,27 @@ class HomeCoordinator: BaseCoordinator {
     // MARK: Overridden Functions
 
     override func start() {
-        let viewController = HomeViewController.generateController()
-        guard let homeViewController = viewController as? HomeViewController else { return }
-
-        self.navigationController.setViewControllers([homeViewController], animated: true)
+        self.buildViewModelBindings()
     }
 
     // MARK: Functions
 
-    func navigateToHomeSearchScreen() {
+    private func buildViewModelBindings() {
+        self.bindShouldNavigateToSearchScreenResult()
+    }
+
+    private func bindShouldNavigateToSearchScreenResult() {
+        self.viewModel?.shouldNavigateToSearchScreen
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] result in
+                guard let self else { return }
+
+                if result { self.navigateToHomeSearchScreen() }
+            })
+            .disposed(by: self.disposeBag)
+    }
+
+    private func navigateToHomeSearchScreen() {
         let coordinator = HomeSearchCoordinator.intance
         coordinator.navigationController = self.navigationController
         self.start(coordinator: coordinator)
