5d102755869ba1ae7452dc13eb460302d3a4c8d7
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2024-11-29 15:27:28 +0700

    chore: add base functionalities for Coordinator

--- WeNews/WeNews/Coordinators/BaseCoordinator.swift
+++ WeNews/WeNews/Coordinators/BaseCoordinator.swift
@@ -10,6 +10,8 @@ import UIKit
 class BaseCoordinator: Coordinator {
     // MARK: Properties
 
+    var parentCoordinator: Coordinator?
+    var childCoordinators: [Coordinator] = []
     var navigationController = UINavigationController()
 
     // MARK: Lifecycle
@@ -22,20 +24,42 @@ class BaseCoordinator: Coordinator {
     ///
     func start() { fatalError("start method must be implemented") }
 
-    /// This method usually used in conjunction to show related feature
+    /// Adds the coordinator into the parent's child coordinators
     ///
-    func willStart(coordinator: Coordinator) {
+    func willStart(coordinator: any Coordinator) {
+        self.childCoordinators.append(coordinator)
+
+        coordinator.parentCoordinator = self
         coordinator.start()
+
+        dump(coordinator.parentCoordinator.debugDescription, name: "willStart parent")
+        dump(self.childCoordinators.debugDescription, name: "willStart children")
     }
 
     /// Ends controller flow in related coordinator
     ///
     func finish() { fatalError("finish method must be implemented") }
 
-    /// This method usually used when interaction with current
-    /// child coordinator finish
+    /// Removes the coordinator when interaction with current
+    /// child coordinator finished
     ///
     func willFinish(coordinator: Coordinator) {
-        coordinator.finish()
+        if let index = childCoordinators.firstIndex(where: { $0 === coordinator }) {
+            dump(coordinator.parentCoordinator.debugDescription, name: "willFinish parent")
+            dump(type(of: coordinator), name: "willFinish coordinator")
+
+            coordinator.finish()
+
+            self.childCoordinators.remove(at: index)
+        }
+    }
+
+    /// Removes all child coordinators
+    ///
+    func removeChildCoordinators() {
+        dump(self.parentCoordinator.debugDescription, name: "removeChildCoordinators parent")
+        dump(self.childCoordinators.debugDescription, name: "removeChildCoordinators children")
+
+        self.childCoordinators.forEach { self.willFinish(coordinator: $0) }
     }
 }
