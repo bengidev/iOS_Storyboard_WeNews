d0d544927384bbed434f0dcd6909d6268553198e
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2024-11-29 15:32:10 +0700

    chore: add required methods to adopts Coordinator

--- WeNews/WeNews/Coordinators/Coordinator.swift
+++ WeNews/WeNews/Coordinators/Coordinator.swift
@@ -9,9 +9,11 @@ import UIKit
 
 protocol Coordinator: AnyObject {
     var navigationController: UINavigationController { get set }
+    var parentCoordinator: Coordinator? { get set }
 
     func start()
     func willStart(coordinator: Coordinator)
     func finish()
     func willFinish(coordinator: Coordinator)
+    func removeChildCoordinators()
 }
