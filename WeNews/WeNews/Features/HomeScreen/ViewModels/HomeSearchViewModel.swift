dbc914ca3e7a25a73f304d34c8615adf19869bd4
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2024-12-02 16:18:45 +0700

    chore: send request API when text changed
    details:
    - ViewModel will send API request when Search Bar text changed after 3 seconds

--- WeNews/WeNews/Features/HomeScreen/ViewModels/HomeSearchViewModel.swift
+++ WeNews/WeNews/Features/HomeScreen/ViewModels/HomeSearchViewModel.swift
@@ -12,17 +12,65 @@ import RxSwift
 class HomeSearchViewModel {
     // MARK: Properties
 
-    let shouldBackToHomeScreen = PublishSubject<Bool>()
+    private(set) var searchBarTextObservable = BehaviorSubject<String>(value: "")
+    private(set) var apiSearchResultObservable = PublishSubject<[SearchNews]>()
+    private(set) var didTapBackButtonObservable = PublishSubject<Void>()
+
+    private let apiSource: CurrentsAPISource
 
     private let disposeBag = DisposeBag()
 
     // MARK: Lifecycle
 
-    init() {}
+    init(apiSource: CurrentsAPISource) {
+        self.apiSource = apiSource
+
+        self.buildControllerBindings()
+    }
 
     // MARK: Functions
 
-    func setShouldBackToHomeScreen(to value: Bool) {
-        self.shouldBackToHomeScreen.onNext(value)
+    func resetViewModelObservables() {
+        self.apiSearchResultObservable = PublishSubject<[SearchNews]>()
+        self.didTapBackButtonObservable = PublishSubject<Void>()
+    }
+
+    private func buildControllerBindings() {
+        self.bindSearchTextObservable()
+    }
+
+    private func bindSearchTextObservable() {
+        self.searchBarTextObservable
+            .skip(while: { $0.isEmpty })
+            .distinctUntilChanged()
+            .debounce(.seconds(3), scheduler: MainScheduler.instance)
+            .flatMap { [weak self] text -> Observable<CurrentNews> in
+                guard let self else { return .empty() }
+
+                return self.apiSource.sendGetSearchNews(withKeywords: text)
+                    .retry(3)
+                    .catchAndReturn(.empty)
+            }
+            .observe(on: ConcurrentDispatchQueueScheduler(qos: .background))
+            .map { currentNews -> [SearchNews] in
+                var tempNews = currentNews.news.map { return SearchNews(image: $0.image, title: $0.title, body: $0.description) }
+                var searchNews: [SearchNews] = []
+
+                forNews: for news in tempNews {
+                    if searchNews.count == 10 { break forNews }
+
+                    searchNews.append(news)
+                }
+
+                return searchNews
+            }
+            .debug()
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] result in
+                guard let self else { return }
+
+                dump(result.count, name: "bindSearchTextObservable")
+            })
+            .disposed(by: self.disposeBag)
     }
 }
