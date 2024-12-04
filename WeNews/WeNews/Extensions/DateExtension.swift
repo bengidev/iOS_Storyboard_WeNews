a379d524071b28ddf25ca5119ca46ab41548394a
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2024-12-04 15:36:12 +0700

    chore: add extension for convert Date to and from

--- WeNews/WeNews/Extensions/DateExtension.swift
+++ WeNews/WeNews/Extensions/DateExtension.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+extension Date {
+    func localeFormatted() -> String {
+        let formatter = DateFormatter()
+
+        formatter.calendar = Calendar(identifier: .iso8601)
+        formatter.locale = .current
+        formatter.timeZone = .current
+        formatter.dateFormat = "dd/M/yyyy, H:mm"
+
+        return formatter.string(from: self)
+    }
+}
--- WeNews/WeNews/Extensions/StringExtension.swift
+++ WeNews/WeNews/Extensions/StringExtension.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+extension String {
+    func dateFormatted(
+        with dateFormat: String = "dd/M/yyyy, H:mm",
+        calendar: Calendar = Calendar(identifier: .iso8601),
+        defaultDate: Date? = nil, locale:
+        Locale = .current,
+        timeZone: TimeZone = .current
+    ) -> Date? {
+        let formatter = DateFormatter()
+
+        formatter.calendar = calendar
+        formatter.defaultDate = defaultDate ?? calendar.date(bySettingHour: 12, minute: 0, second: 0, of: Date())
+        formatter.locale = locale
+        formatter.timeZone = timeZone
+        formatter.dateFormat = dateFormat
+
+        return formatter.date(from: self)
+    }
+}
