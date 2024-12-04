0ef2fb12dc5002347dbc9398ee41e263cb3d75e9
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2024-12-04 14:30:42 +0700

    chore: add extension for shadow corner UIImageView

--- WeNews/WeNews/Extensions/UIImageViewExtension.swift
+++ WeNews/WeNews/Extensions/UIImageViewExtension.swift
@@ -5,4 +5,20 @@
 //  Created by ENB Mac Mini M1 on 04/12/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView {
+    func applyshadowWithCorner(containerView: UIView, cornerRadious: CGFloat) {
+        containerView.backgroundColor = .clear
+        containerView.layer.shadowColor = UIColor.black.cgColor
+        containerView.layer.shadowOpacity = 1.0
+        containerView.layer.shadowOffset = .zero
+        containerView.layer.shadowRadius = 5.0
+        containerView.layer.shouldRasterize = true
+        containerView.layer.rasterizationScale = UIScreen.main.scale
+        containerView.layer.masksToBounds = false
+
+        self.layer.cornerRadius = 10.0
+        self.layer.masksToBounds = true
+    }
+}
