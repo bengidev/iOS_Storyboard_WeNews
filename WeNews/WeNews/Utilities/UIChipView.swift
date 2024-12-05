d86013339c3186a0331ce622bae8f2c136032170
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2024-12-05 11:19:54 +0700

    chore: add UISegmentedChipView in Utilities

---
+++ WeNews/WeNews/Utilities/UIChipView.swift
@@ -0,0 +1,108 @@
+//
+//  UIChipView.swift
+//  WeNews
+//
+//  Created by ENB Mac Mini M1 on 05/12/24.
+//
+
+import Foundation
+import UIKit
+
+final class UIChipView: UIView {
+    // MARK: Properties
+
+    var didSelect: ((String) -> Void)?
+
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.5
+        label.textColor = UIColor.darkGray
+        label.font = .preferredFont(forTextStyle: .subheadline)
+        return label
+    }()
+
+    private lazy var backgroundContainerView: UIView = {
+        let view = UIView()
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor.lightGray
+        view.layer.cornerRadius = 5.0
+        view.clipsToBounds = true
+        return view
+    }()
+
+    // MARK: Computed Properties
+
+    var isSelected: Bool = false {
+        didSet {
+            self.updateSelection()
+        }
+    }
+
+    // MARK: Lifecycle
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.setupView()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        self.setupView()
+    }
+
+    // MARK: Functions
+
+    func setupView() {
+        self.addSubview(self.backgroundContainerView)
+        NSLayoutConstraint.activate([
+            self.backgroundContainerView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.backgroundContainerView.topAnchor.constraint(equalTo: self.topAnchor),
+            self.backgroundContainerView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            self.backgroundContainerView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+        ])
+
+        self.backgroundContainerView.addSubview(self.titleLabel)
+        NSLayoutConstraint.activate([
+            self.titleLabel.leadingAnchor.constraint(equalTo: self.backgroundContainerView.leadingAnchor, constant: 8.0),
+            self.titleLabel.topAnchor.constraint(equalTo: self.backgroundContainerView.topAnchor, constant: 8.0),
+            self.titleLabel.rightAnchor.constraint(equalTo: self.backgroundContainerView.rightAnchor, constant: -8.0),
+            self.titleLabel.bottomAnchor.constraint(equalTo: self.backgroundContainerView.bottomAnchor, constant: -8.0),
+        ])
+
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleGesture))
+        self.backgroundContainerView.addGestureRecognizer(tap)
+        self.backgroundContainerView.isUserInteractionEnabled = true
+    }
+
+    func setup(title: String) {
+        self.titleLabel.text = title
+    }
+
+    @objc func handleGesture() {
+        self.didSelect?(self.titleLabel.text ?? "")
+    }
+
+    private func updateSelection() {
+        DispatchQueue.main.async { [weak self] in
+            guard let self else { return }
+
+            UIView.animate(withDuration: 0.1, delay: 0.0, options: .transitionCrossDissolve) {
+                if self.isSelected {
+                    self.titleLabel.textColor = UIColor.black
+                    self.titleLabel.font = .preferredFont(forTextStyle: .headline)
+                    self.backgroundContainerView.backgroundColor = UIColor.lightGray
+                } else {
+                    self.titleLabel.textColor = UIColor.darkGray
+                    self.titleLabel.font = .preferredFont(forTextStyle: .subheadline)
+                    self.backgroundContainerView.backgroundColor = UIColor.lightGray.withAlphaComponent(0.5)
+                }
+            }
+        }
+    }
+}
---
+++ WeNews/WeNews/Utilities/UISegmentedChipView.swift
@@ -0,0 +1,165 @@
+//
+//  UISegmentedChipView.swift
+//  WeNews
+//
+//  Created by ENB Mac Mini M1 on 05/12/24.
+//
+
+import UIKit
+
+// MARK: - UISegmentedChipViewDelegate
+
+protocol UISegmentedChipViewDelegate: AnyObject {
+    func didTapIndex(index: Int, str: String)
+}
+
+// MARK: - UISegmentedChipView
+
+final class UISegmentedChipView: UIView {
+    // MARK: Properties
+
+    weak var delegate: UISegmentedChipViewDelegate?
+    lazy var scrollView: UIScrollView = {
+        let view = UIScrollView()
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.bounces = false
+        view.delaysContentTouches = false
+        view.showsHorizontalScrollIndicator = false
+        view.showsVerticalScrollIndicator = false
+        return view
+    }()
+
+    private var chipsTitle: [String] = []
+    private var selectedItemIndex: Int?
+    private var chipViews: [UIChipView] = []
+
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .clear
+        return view
+    }()
+
+    private lazy var filterStackView: UIStackView = {
+        let view = UIStackView()
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.axis = .horizontal
+        view.alignment = .fill
+        view.distribution = .fill
+        view.spacing = 12
+        return view
+    }()
+
+    // MARK: Lifecycle
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.configUI()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        self.configUI()
+    }
+
+    // MARK: Functions
+
+    public func setup(chipsTitle: [String]) {
+        self.chipsTitle = chipsTitle
+        self.setupChipStackView()
+    }
+
+    func configUI() {
+        self.addSubview(self.scrollView)
+        NSLayoutConstraint.activate([
+            self.scrollView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.scrollView.topAnchor.constraint(equalTo: self.topAnchor),
+            self.scrollView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            self.scrollView.heightAnchor.constraint(equalToConstant: 40.0),
+        ])
+
+        self.scrollView.addSubview(self.containerView)
+        NSLayoutConstraint.activate([
+            self.containerView.leadingAnchor.constraint(equalTo: self.scrollView.leadingAnchor),
+            self.containerView.topAnchor.constraint(equalTo: self.scrollView.topAnchor),
+            self.containerView.trailingAnchor.constraint(equalTo: self.scrollView.trailingAnchor),
+            self.containerView.bottomAnchor.constraint(equalTo: self.scrollView.bottomAnchor),
+            self.containerView.heightAnchor.constraint(equalTo: self.scrollView.heightAnchor),
+        ])
+
+        self.containerView.addSubview(self.filterStackView)
+        NSLayoutConstraint.activate([
+            self.filterStackView.leadingAnchor.constraint(equalTo: self.containerView.leadingAnchor, constant: 20.0),
+            self.filterStackView.topAnchor.constraint(equalTo: self.containerView.topAnchor),
+            self.filterStackView.trailingAnchor.constraint(equalTo: self.containerView.trailingAnchor, constant: -20.0),
+            self.filterStackView.bottomAnchor.constraint(equalTo: self.containerView.bottomAnchor),
+        ])
+    }
+
+    func titleToWidth(text: String) -> CGFloat {
+        let size = (text as NSString).boundingRect(
+            with: CGSize(width: CGFloat.infinity, height: CGFloat.infinity),
+            options: .usesLineFragmentOrigin,
+            attributes: [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .footnote)],
+            context: nil
+        )
+
+        return size.width + 20.0
+    }
+
+    private func setupChipStackView() {
+        self.chipViews.removeAll()
+        self.filterStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
+
+        for (index, title) in self.chipsTitle.enumerated() {
+            let chip = UIChipView()
+            chip.setup(title: title)
+            chip.didSelect = { [weak self] title in
+                guard let self else { return }
+
+                self.selectChip(at: index)
+                self.delegate?.didTapIndex(index: index, str: title)
+            }
+            self.filterStackView.addArrangedSubview(chip)
+            self.chipViews.append(chip)
+
+            NSLayoutConstraint.activate([
+                chip.widthAnchor.constraint(equalToConstant: self.titleToWidth(text: title)),
+            ])
+
+            layoutIfNeeded()
+        }
+
+        if !self.chipViews.isEmpty {
+            self.selectChip(at: 0)
+        }
+    }
+
+    private func selectChip(at index: Int) {
+        self.selectedItemIndex = index
+        for (currentIndex, chip) in self.chipViews.enumerated() {
+            chip.isSelected = (currentIndex == index)
+        }
+        self.scrollToSelectedChip()
+    }
+
+    private func scrollToSelectedChip() {
+        DispatchQueue.main.async { [weak self] in
+            guard let self else { return }
+            guard let selectedItemIndex = selectedItemIndex else { return }
+
+            UIView.animate(withDuration: 0.1, delay: 0.0, options: .transitionCrossDissolve) {
+                let selectedChip = self.chipViews[selectedItemIndex]
+                let chipFrame = selectedChip.frame
+                let scrollViewFrame = self.scrollView.frame
+                let targetContentOffsetX = chipFrame.midX - scrollViewFrame.width / 2
+
+                let contentOffsetX = max(0, min(self.scrollView.contentSize.width - scrollViewFrame.width, targetContentOffsetX))
+                self.scrollView.setContentOffset(CGPoint(x: contentOffsetX, y: 0), animated: true)
+            }
+        }
+    }
+}
