a652ca69a6ec989eebfef558b73861197fa04ec0
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2024-12-06 16:29:13 +0700

    refactor: change API sources to match parameters

--- WeNews/WeNews/DataSources/NewsAPISource/NewsAPISource.swift
+++ WeNews/WeNews/DataSources/NewsAPISource/NewsAPISource.swift
@@ -24,25 +24,26 @@ class NewsAPISource {
 
     // MARK: Functions
 
-    func sendGetTopHeadlines(withKeywords keywords: String, forPage page: Int) -> Observable<News> {
-        guard !keywords.isEmpty else { return .empty() }
+    func sendGetTopHeadlines(withCategory category: String, forPage page: Int) -> Observable<News> {
+        guard !category.isEmpty else { return .empty() }
 
         let headers = [
             "Authorization": "955941bd6474428ba0032ea83c63d402",
         ]
 
         let request = NSMutableURLRequest()
-        request.url = .init(string: self.baseURL + "/top-headlines?q=\(keywords)&searchIn=title,content&sortBy=relevancy&page=\(page)")
+        request.url = .init(string: self.baseURL + "/top-headlines?category=\(category)&page=\(page)")
         request.allHTTPHeaderFields = headers
         request.cachePolicy = .useProtocolCachePolicy
         request.timeoutInterval = 5.0
         request.httpMethod = "GET"
 
-        return URLSession.shared.rx.data(request: request as URLRequest)
+        return URLSession.shared.rx.continueData(request: request as URLRequest)
+            .catchAndReturn(.init())
+            .observe(on: ConcurrentDispatchQueueScheduler(qos: .background))
             .map { data -> News in
                 return try newJSONDecoder().decode(News.self, from: data)
             }
-            .observe(on: ConcurrentDispatchQueueScheduler(qos: .background))
     }
 
     func sendGetSearchNews(withKeywords keywords: String, forPage page: Int) -> Observable<News> {
@@ -60,9 +61,10 @@ class NewsAPISource {
         request.httpMethod = "GET"
 
         return URLSession.shared.rx.data(request: request as URLRequest)
+            .catchAndReturn(.init())
+            .observe(on: ConcurrentDispatchQueueScheduler(qos: .background))
             .map { data -> News in
                 return try newJSONDecoder().decode(News.self, from: data)
             }
-            .observe(on: ConcurrentDispatchQueueScheduler(qos: .background))
     }
 }
